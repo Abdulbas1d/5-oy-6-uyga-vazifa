2aa9b4492297d31b5f6b718e59434dde299f9967
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-11-06 21:19:19 +0500

    All cases solved

--- js/index.js
+++ js/index.js
@@ -243,3 +243,386 @@
 // 1-Misol
 
 // Obyektni tartiblang: Obyekt ichidagi kalitlari bo‘yicha tartiblangan yangi obyekt hosil qiling.
+
+// let obj = {
+//     name: "Alisher",
+//     email: "test@gmail.com",
+//     family: {
+//         fatherName: "Farhod",
+//         motherName: "Nargiza",
+//         brotherName: "Akbar"
+//     },
+//     age: 45,
+//     weight: 70 
+// }
+// function sortObjKeys(arg) {
+//     let sortedKeys = Object.keys(arg).sort();
+//     let res = {};
+//     for (const key of sortedKeys) {
+//         res[key] = arg[key]
+//     }
+//     return res
+// }
+// console.log(sortObjKeys(obj));
+
+
+
+
+
+// 2-Misol
+
+// Obyekt kalitlarini va qiymatlarini alohida massivga o‘tkazing: Obyektdagi barcha kalitlar va qiymatlar alohida-alohida massivda qaytarilsin.
+
+// let obj = {
+//     name: "Alisher",
+//     age: 45,
+//     email: "test@gmail.com",
+//     phoneNumber: +498794455555,
+//     weight: 72
+// }
+// function sortKeysAndValues(arg) {
+//     let keys = Object.keys(arg);
+//     let values = Object.values(arg);
+
+//     return {
+//         Kalitlari: keys,
+//         Qiymatlari: values
+//     }
+// }
+// console.log(sortKeysAndValues(obj));
+
+
+
+
+
+// 3-Misol
+
+// Obyekt birlashtirish: Ikkita obyekt berilgan. Ularni birlashtirib, umumiy kalitlarni yangilash orqali natijaviy obyekt yarating.
+
+// let obj1 = {
+//     name: "Alisher",
+//     age: 45,
+//     email: "test@gmail.com",
+//     phoneNumber: +98745632
+// };
+// let obj2 = {
+//     name: "Jahongir",
+//     phoneNumber: +98745632
+// };
+// function twoObjectAdd(arg1, arg2) {
+//     let merge = Object.assign({}, arg1, arg2)
+//     return merge
+// }
+// console.log(twoObjectAdd(obj1, obj2));
+
+
+
+
+// 4-Misol
+
+// Qiymatlarni hisoblash: Berilgan obyekt qiymatlarining umumiy yig‘indisini hisoblang (qiymatlar sonlar bo'lishi kerak).
+
+// let obj = {
+//     name: "Alisher",
+//     age: 54,
+//     email: "test@gmail.com",
+//     weight: 70
+// }
+// function countValuesObj(arg) {
+//     let res = 0;
+//     for (const key in arg) {
+//         if (typeof arg[key] === 'number') {
+//             res += arg[key]
+//         }
+//     }
+
+//     return res
+// }
+// console.log(countValuesObj(obj));
+
+
+
+
+// 5-Misol
+
+// Kalitni qayta nomlash: Obyekt ichidagi bir kalit nomini yangi nomga o'zgartiring va qaytaring.
+
+// let obj = {
+//     name: "Alisher",
+//     age: 45,
+//     email: "test@gmail.com",
+//     phoneNumber: +98745631
+// }
+// function renameKey(arg, oldKey, newKey) {
+//     if (oldKey in arg) {
+//         arg[newKey] = arg[oldKey]
+//         delete arg[oldKey]
+//     }
+//     return arg
+// }
+// console.log(renameKey(obj, "age", "yearsOld"));
+
+
+
+
+
+// 6-Misol
+
+// Kalitni olib tashlash: Berilgan obyekt va kalit orqali kalitni olib tashlab, yangi obyekt qaytaring.
+
+// let obj = {
+//     name: "Alisher",
+//     age: 45,
+//     weight: 70
+// };
+// function removeKey(arg, keyToRemove) {
+//     let newObj = { ...arg };
+//     if (keyToRemove in newObj) {
+//         delete newObj[keyToRemove];
+//     }
+//     return newObj;
+// }
+// console.log(removeKey(obj, "age"));
+
+
+
+
+
+// 7-Misol
+
+// Bir xil kalitli obyektlarni birlashtirish: Berilgan massiv ichidagi obyektlarni bir xil kalit bo‘yicha guruhlab, yangi obyekt yarating.
+
+// let products = [
+//     {name: "apple", quantity: 10, category: "fruit"},
+//     {name: "carrot", quantity: 5, category: "vegetable"},
+//     {name: "banana", quantity: 8, category: "fruit"},
+//     {name: "broccoli", quantity: 6, category: "vegetable"},
+//     {name: "rice", quantity: 20, category: "grain"}
+// ];
+// function findSameKey(arr, key) {
+//     let res = {};
+//     let keyValue = [];
+//     for (let item of arr) {
+//         keyValue = item[key];
+//         if (!res[keyValue]) {
+//             res[keyValue] = [];
+//         }
+//         res[keyValue].push(item);
+//     }
+//     return res;
+// }
+// console.log(findSameKey(products, "category"));
+
+
+
+
+
+// 8-Misol
+
+// Obyekt ichida massivlarni yig'ish: Obyekt ichidagi barcha massivlarni bitta massivga yig‘ing va natijani qaytaring.
+
+// let obj = {
+//     arr1: [1, 2, 3],
+//     name: "Alisher",
+//     isMarried: true ,
+//     arr2: [4, 5, 6]
+// };
+// function collectArrays(obj) {
+//     let res = [];
+//     for (let key in obj) {
+//         if (Array.isArray(obj[key])) {
+//             res = res.concat(obj[key]);
+//         }
+//     }
+//     return res
+// }
+// console.log(collectArrays(obj));
+
+
+
+
+// 9-Misol
+
+// O‘xshash obyektlarni chiqarish: Berilgan ikkita massiv ichidagi obyektlarning umumiy qismlarini toping.
+
+// let arr1 = [
+//     {name: "Alisher", age: 30},
+//     {name: "Akbar", age: 25},
+//     {name: "Anvar", age: 35}
+// ];
+// let arr2 = [
+//     {name: "Akbar", age: 25},
+//     {name: "Anvar", age: 35},
+//     {name: "Jamshid", age: 40}
+// ];
+// function findCommonObj(arr1, arr2) {
+//     const commonObj = []; 
+//     for (let obj1 of arr1) {
+//         for (let obj2 of arr2) {
+//             if (isEqual(obj1, obj2)) { 
+//                 commonObj.push(obj1);
+//                 break;
+//             }
+//         }
+//     }
+//     return commonObj;
+// }
+// function isEqual(obj1, obj2) {
+//     let keys1 = Object.keys(obj1);
+//     let keys2 = Object.keys(obj2);
+//     if (keys1.length !== keys2.length) {
+//         return false;
+//     }
+//     for (let key of keys1) {
+//         if (obj1[key] !== obj2[key]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// console.log(findCommonObj(arr1, arr2));
+
+
+
+
+
+// 10-Misol
+
+// Obyektning chuqurligini aniqlash: Obyekt ichma-ich obyektlardan tashkil topgan bo‘lsa, uning chuqurligini (depth) topadigan funksiya yozing.
+
+// let obj = {
+//     name: "Alisher",
+//     age: 40,
+//     family: {
+//         fatherName: "Alijon",
+//         motherName: "Gulchexra",
+//         sisterFamily: {
+//             sisterSonName: "Ali",
+//             sisterHusbandName: "Shuxrat",
+//             sisterMotherInLawFamily: {
+//                 motherInLawName: "Sevara",
+//                 fatherInLawName: "Farxod",
+//             }
+//         }
+//     }
+// }
+// function getObjDepth(arg) {
+//     let maxDepth = 1;
+//     let depth;
+//     for (let key in arg) {
+//         if (typeof arg[key] === "object" && arg[key] !== null) { 
+//             depth = getObjDepth(arg[key]) + 1;
+//             maxDepth = Math.max(maxDepth, depth);
+//         }
+//     }
+
+//     return maxDepth;
+// }
+// console.log(getObjDepth(obj));
+
+
+
+
+
+
+
+
+//////////----- String bilan ishlash -----//////////
+
+
+
+
+
+// 1-Misol
+
+// Palindromni tekshirish: Berilgan so‘z yoki jumlaning palindrom ekanligini tekshiruvchi funksiya yozing (bo‘sh joy va katta-kichik harflarni inobatga olmang).
+
+// let str = "A man, a plan, a canal, Panama";
+// function isPalindrome(str) {
+//     str = str.toLowerCase();
+//     let cleanedStr = "";
+//     for (let i = 0; i < str.length; i++) {
+//         if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= '0' && str[i] <= '9')) {
+//             cleanedStr += str[i];
+//         }
+//     }
+//     let reversedStr = cleanedStr.split('').reverse().join('');
+//     return cleanedStr === reversedStr;
+// }
+// console.log(isPalindrome(str));
+
+
+
+
+
+// 2-Misol
+
+// So‘zlarni tersga o‘girish: Berilgan jumladagi so‘zlarni teskari tartibda joylashtiruvchi funksiya yozing.
+
+// let str = "Assalomu alaykum";
+// function reverseStr(str) {
+//     return str.split('').reverse().join(' ');
+// }
+// console.log(reverseStr(str));
+
+
+
+
+// 3-Misol
+
+// Eng uzun so‘zni toping: Berilgan jumladagi eng uzun so‘zni topib, uni qaytaruvchi funksiya yozing.
+
+// let fruits = ["Apelsin", "Gilos", "Olma", "Banan", "Qulupnay"];
+// function findBigLenthElement(str) {
+//     let max = "";
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] > max) {
+//             max = str[i]
+//         }
+//     }
+//     return max
+// }
+// console.log(findBigLenthElement(fruits));
+
+
+
+
+
+// 4-Misol
+
+// Bir harfni ko‘paytirish: Berilgan string ichidagi ma’lum bir harfni ko‘paytirish kerak (masalan, har bir `a` harfidan keyin qo‘shimcha `a` qo‘shing).
+
+// let str = "Assalomu alaykum";
+// let char = 'a'
+// function repeatChar(str, char) {
+//     let res = "";
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === char) {
+//             res += char + char
+//         } else {
+//             res += str[i] 
+//         }
+//     }
+//     return res
+// }
+// console.log(repeatChar(str, char));
+
+
+
+
+
+// 5-Misol
+
+// Stringdan raqamlarni olib tashlash: Berilgan string ichidan barcha raqamlarni olib tashlab, qolgan qismni qaytaring.
+
+// let str = "Assa1lo2mu4 al9ay6k4um";
+// function removeNumberInString(str) {
+//     let res = "";
+//     for (let i = 0; i < str.length; i++) {
+//         if (!(str[i] >= "0" && str[i] <= "9")) {
+//             res += str[i]
+//         }
+//     }
+//     return res
+// }
+// console.log(removeNumberInString(str));
