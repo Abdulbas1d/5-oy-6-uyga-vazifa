7478bacde4d145a8d8525084fa37445b23a3e1c0
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-11-06 15:02:59 +0500

    Any codes write

--- js/index.js
+++ js/index.js
@@ -118,3 +118,128 @@
 
 
 // 6-Misol
+
+// Massivni ichma-ich massivlardan tozalang: Ichma-ich massivlardan iborat massiv berilgan. Uni faqat bir darajali massivga o‘tkazing (Flatten array).
+
+// let arr = [1, 2, [5, 6], [8, 9], 9, 10];
+// function allMassivOnlyDoneOne(arg) {
+//     let res = [];
+//     let i = 0;
+//     while (i < arg.length) {
+//         if (Array.isArray(arg[i])) {
+//             arg = arg.slice(0, i).concat(arg[i], arg.slice(i + 1));
+//         } else {
+//             res.push(arg[i]);
+//             i++;
+//         }
+//     }
+//     return res;
+// }
+// console.log(allMassivOnlyDoneOne(arr));
+
+
+
+
+// 7-Misol
+
+// Ikki massivni birlashtiring: Ikkita massiv berilgan, ularni birlashtirib, takroriy elementlarni olib tashlang.
+
+// let arr1 = [1, 2, 3, 4, 5];
+// let arr2 = [1, 6, 7, 8, 5];
+// function concatTwoArray(arg1, arg2) {
+//     let addTwoArr = arg1.concat(arg2)
+//     let res = [];
+//     for (let i = 0; i < addTwoArr.length; i++) {
+//         if(!res.includes(addTwoArr[i])) {
+//             res.push(addTwoArr[i])
+//         }
+//     }
+//     return res
+// }
+// console.log(concatTwoArray(arr1, arr2));
+
+
+
+
+
+// 8-Misol
+
+// Massiv elementlarini toq/juft qilib ajrating: Massivdagi toq va juft elementlarni alohida massivlarga ajratib qaytaring.
+
+// let arr = [1, 22, 33, 14, 51, 86, 99, 18];
+// function sortArray(arg) {
+//     let oddNum = [];
+//     let evenNum = [];
+//     for (let i = 0; i < arg.length; i++) {
+//         if (arg[i] % 2 == 0) {
+//             evenNum.push(arg[i])
+//         } else {
+//             oddNum.push(arg[i])
+//         }
+//     }
+//     return {
+//         ToqSonlar: oddNum,
+//         JuftSonlar: evenNum
+//     }
+// }  
+// console.log(sortArray(arr));
+
+
+
+
+// 9-Misol
+
+// Massivda ma'lum qiymat borligini tekshirish: Massiv va qiymat berilgan. Funksiya massiv ichida qiymat mavjud bo‘lsa `true`, aks holda `false` qaytarsin.
+
+// let arr = [1, 2, 3, 4];
+// let value = 3;
+// function hasInArray(arg, value) {
+//     for (let i = 0; i < arg.length; i++) {
+//         if (arg[i] === value) {
+//             return true;
+//         }
+//     }
+//     return false
+// }
+// console.log(hasInArray(arr, value));
+
+
+
+
+// 10-Misol
+
+// Massivdagi eng katta va eng kichik elementni toping: Massivdagi eng katta va eng kichik elementlarni qaytaruvchi funksiya yozing.
+
+// let arr = [1, 39, 45, 78, 54, 17];
+// function findMaxNumAndMinNum(arg) {
+//     let max = arg[0];
+//     let min = arg[0];
+//     for (let i = 1; i < arg.length; i++) {
+//         if (arg[i] > max) {
+//             max = arg[i]
+//         }
+//         if (arg[i] < min) {
+//             min = arg[i]
+//         }
+//     }
+//     return {
+//         KattaSon: max,
+//         KichikSon: min
+//     }
+// }
+// console.log(findMaxNumAndMinNum(arr));
+
+
+
+
+
+
+
+
+//////////----- Obyektlar bilan ishlash -----//////////
+
+
+
+// 1-Misol
+
+// Obyektni tartiblang: Obyekt ichidagi kalitlari bo‘yicha tartiblangan yangi obyekt hosil qiling.
